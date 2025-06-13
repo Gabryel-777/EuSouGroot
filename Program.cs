04f62eef11473ecf83fb61291a125ad06a65ef05
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-13 20:04:00 -0300

    EU SOU O GROOT!

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"
+    +---------------------+
+    |   EU SOU O GROOT!   |
+    +---------------------+
+        ");
+
+        while (true)
+        {
+            Console.Write("Pergunta: ");
+            string? pergunta = Console.ReadLine();
+
+            
+            if (string.IsNullOrWhiteSpace(pergunta))
+            {
+                continue; 
+            }
+
+            if (pergunta.ToLower().Trim() == "tchau")
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("                          Resposta: Eu sou Groot!");
+                Console.ResetColor();
+
+                break;
+
+            }
+            else
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("                          Resposta: Eu sou Groot.");
+                Console.ResetColor();
+
+            }
+        }
+    }
+}
